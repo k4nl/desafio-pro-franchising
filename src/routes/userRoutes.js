59c50b5a5febfe5fb9680a8740926318863ff3f8
k4nl <gustavoh.bh@gmail.com>
2022-02-18 16:25:46 -0300

    criando a rota para login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const userControllers = require('../controllers/userControllers');
 
 router.post('/sign', userControllers.createUser);
+router.post('/', userControllers.login);
 
 module.exports = router;
