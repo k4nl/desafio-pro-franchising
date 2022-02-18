08ed1eda6076b17f5a951a619e8bfd83fafbd1c9
k4nl <gustavoh.bh@gmail.com>
2022-02-18 13:39:25 -0300

    setando teste para o userCreate e para a connection do mongodb

---
+++ src/tests/unit/userModels.test.js
@@ -0,0 +1,39 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const connection = require('./mongoConnection');
+
+const userModels = require('../../models/userModels')
+
+const userExample = {
+	name: 'Seu ze',
+	email: 'seuze@lojinhadoseuze.com',
+	password: 'seuze123',
+};
+
+describe('Testing userModel', () => {
+  let connectionMock;
+  
+  beforeEach(async () => {
+    connectionMock = await connection();
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+    await connectionMock.db('SeuzeStore').collection('users').insertOne(
+      {'user': { 'name': 'Seu ze', 'email': 'seuze@lojinhadoseuze.com', 'password': 'seuze123', 'role': 'admin' } },
+    )
+  });
+
+  afterEach(async () => {
+    await connectionMock.db('SeuzeStore').collection('users').drop();
+    await MongoClient.connect.restore();
+  });
+
+  describe('Testing o userModels.create', () => {
+    it('If it is possible to create an user', async () => {
+      const { ops } = await userModels.createUser(userExample);
+      const user = ops[0].user;
+      expect(user).to.not.be.null;
+      expect(user).to.deep.equal(userExample);
+    });
+  });
+});
---
+++ src/utils/dictionary/status.js
@@ -0,0 +1,9 @@
+module.exports = {
+  success: 200,
+  serverError: 500,
+  created: 201,
+  badRequest: 400,
+  invalidRequest: 422,
+  notFound: 404,
+  unauthorized: 401,
+}
