87a9bf97b01e6ae5faf1316a075fd934182049f1
k4nl <gustavoh.bh@gmail.com>
2022-02-18 15:52:28 -0300

    criando as funcoes de teste para o servico de login

--- src/tests/unit/userServices.test.js
+++ src/tests/unit/userServices.test.js
@@ -21,6 +21,21 @@ const userExample = {
   role: 'admin',
 };
 
+const userLogin = {
+	email: 'seuze@lojinhadoseuze.com',
+	password: 'seuze123',
+};
+
+const userNotFound = {
+	email: 'emailnotregisted@email.com',
+	password: 'seuze123',
+};
+
+const incorrectPassword = {
+	email: 'seuze@lojinhadoseuze.com',
+	password: 'incorrectPassword',
+};
+
 describe('Testing userServices', () => {
 
   describe('Testing createUser', () => {
@@ -38,7 +53,7 @@ describe('Testing userServices', () => {
       });
       
       it('It should returns the created user', async () => {
-        const userCreated = await userServices.createUser(userExample);
+        const userCreated = await userServices.createUser(userLogin);
 
         expect(userCreated).to.be.a('object');
         expect(userCreated).to.have.property('_id');
@@ -116,6 +131,106 @@ describe('Testing userServices', () => {
               await userServices.createUser({ name: 'seuze', password: '123', email: 'seuze@email.com', role: 123 });
           } catch (error) {
             expect(error).to.deep.equal(new CustomError({ message: '"role" must be a string', status: s.invalidRequest }));
+          }
+        });
+      });
+    });
+  });
+
+/* ########################## LOGIN SERVICES  ####################################*/
+
+  describe('Testing login', () => {
+
+    describe('When its possible to login', () => {
+      
+      beforeEach(() => {
+        sinon.stub(userModels, 'findUserByEmail').resolves(userFound);
+      });
+
+      afterEach(() => {
+        userModels.findUserByEmail.restore();
+      });
+      
+      it('It should returns login token', async () => {
+        const data = await userServices.login(userExample);
+
+        expect(data).to.be.a('object');
+        expect(data).to.have.property('token');
+        expect(data).to.have.property('user');
+        expect(data.user).to.not.have.property('_id');
+        expect(data.user).to.not.have.property('password');
+        expect(data.user).to.not.have.property('role');
+        expect(data.user.name).to.deep.equal(userExample.name);
+        expect(data.user.email).to.deep.equal(userExample.email);
+      });
+    });
+
+    describe('When it finds cant find an email', () => {
+      
+      beforeEach(() => {
+        sinon.stub(userModels, 'findUserByEmail').resolves(null);
+      });
+
+      afterEach(() => {
+        userModels.findUserByEmail.restore();
+      });
+      
+      it('It should return an error', async () => {
+        try {
+            await userServices.login(userNotFound);
+        } catch (error) {
+          expect(error).to.deep.equal(new CustomError(e.userNotFound));
+        }
+      });
+    });
+
+    describe('When it has a wrong password', () => {
+      
+      beforeEach(() => {
+        sinon.stub(userModels, 'findUserByEmail').resolves(userExample);
+      });
+
+      afterEach(() => {
+        userModels.findUserByEmail.restore();
+      });
+      
+      it('It should return an error', async () => {
+        try {
+            await userServices.login(incorrectPassword);
+        } catch (error) {
+          expect(error).to.deep.equal(new CustomError(e.incorrectPassword));
+        }
+      });
+    });
+
+    describe('When it receives an invalid data', () => {
+
+      describe('When password it is not a string', () => {
+        it('It should return an error', async () => {
+          try {
+              await userServices.login({ password: 123, email: 'email@email.com' });
+          } catch (error) {
+            expect(error).to.deep.equal(new CustomError({ message: '"password" must be a string', status: s.invalidRequest }));
+          }
+        });
+      });
+
+      describe('When email it is not a string', () => {
+        it('Deve retornar um erro', async () => {
+          try {
+              await userServices.createUser({ password: '123', email: 1234 });
+          } catch (error) {
+            expect(error).to.deep.equal(new CustomError({ message: '"email" must be a string', status: s.invalidRequest }));
+          }
+        });
+      });
+
+      describe('When email it is not a valid email', () => {
+        it('Deve retornar um erro', async () => {
+          try {
+              await userServices.createUser({ password: '123', email: 'seuze' });
+          } catch (error) {
+            expect(error).to.deep.equal(new CustomError({ message: '"email" must be a valid email', status: s.invalidRequest }));
           }
         });
       });
--- src/utils/dictionary/errorObjects.js
+++ src/utils/dictionary/errorObjects.js
@@ -2,6 +2,10 @@ const s = require('./status');
 
 const error = {
   emailAlreadyExist: { status: s.alreadyExists, message: 'Email already exists, please choose another email' },
+  userNotFound: { status: s.notFound, message: 'User not found'},
+  incorrectPassword: {status: s.unauthorized, message: 'Incorrect Password'},
 }
+
+
 
 module.exports = error;
